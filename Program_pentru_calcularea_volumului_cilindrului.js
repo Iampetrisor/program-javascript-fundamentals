554b0fc1c1d6b4cfa8ae033a917f4e132934e8fe
Iampetrisor <teo.petri@yahoo.com>
2023-05-15 14:39:22 +0300

    scurt
    actualizat

--- Program_pentru_calcularea_volumului_cilindrului.js
+++
@@ -1,5 +0,0 @@
-var r = prompt("Introduceti raza bazei cilindrului:");
-var H = prompt("Introduceti inaltimea cilindrului:");
-const Pi = 3,14;
-var V = r*r*H*Pi;
-alert("Volumul Cilindrului este:" V);
